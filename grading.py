5bad31b6293d20958ebc05149ea42bdd6ad1276a
legend550 <100070323+legend550@users.noreply.github.com>
2022-05-09 19:07:34 +0530

    grades in A+ , B , C , D ,E

---
+++ grading.py
@@ -0,0 +1,16 @@
+marks = int(input("enter your marks below under 100:\n"))
+
+
+if marks>=90 and marks<=100:
+    print("your grade is = A+")
+elif marks>=80:
+    print("your grade is = B")
+elif marks>=70:
+    print("your grade is = C")
+elif marks>=60:
+    print("your grade is = D")
+elif marks >= 50:
+    print("your grade is = E")
+else:
+    print("SORRY, YOUR ARE FAIL")
+    print("तू काबिल नही है")
