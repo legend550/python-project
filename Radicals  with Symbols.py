209cf1c28a4f7eceb864b49bab68b7e19558f424
legend550 <100070323+legend550@users.noreply.github.com>
2022-05-23 20:50:13 +0530

    Add files via upload

---
+++ Radicals  with Symbols.py
@@ -0,0 +1,115 @@
+print("WELCOME")
+print("Choose the Element to get its Name and Valency \a")
+print("H\t","OH\t","Nh4\t","Cu\t","Cl\t","So4\t","Po4\t","Co3\t","HCo3\t","S\t","P\t","No3\t","No2\t","Sn\t","Pb\t","MnO4\n","CR2O7\t","K\t","Ca\t","Mg\t","Li\t","Zn\t","Mn\t","F\t","CH3COO\t","Hg\t","Br\t","I\t","C6H5COO\t","O\t","O2\t")
+name = input("Write same as it is given above \n")
+if name == ("H"):
+    print("Name of", name , "is Hydride ion")
+    print("Valency of",name,"is----> -1")
+elif name == ("OH"):
+    print("Name of", name , "is Hydroxide ion")
+    print("Valency of",name,"is----> -1")
+elif name == ("Nh4"):
+    print("Name of", name , "is Ammonium ion")
+    print("Valency of",name,"is----> +1")
+elif name == ("Cu"):
+    print("Name of this ion when its valency is +1 that is Cupress ion")
+    print("Name of this ion when its valency is +2 that is Cupric ion")
+elif name == ("Cl"):
+    print("Name of", name , "is Chloride ion")
+    print("Valency of",name,"is----> -1")
+elif name == ("So4"):
+    print("Name of", name , "is Sulphate ion")
+    print("Valency of",name,"is----> -2")
+elif name == ("Po4"):
+    print("Name of", name , "is Phosphate ion")
+    print("Valency of",name,"is----> -3")
+elif name == ("Co3"):
+    print("Name of", name , "is Carbonate ion")
+    print("Valency of",name,"is----> -2")
+elif name == ("HCo3"):
+    print("Name of", name , "is Bicarbonate ion")
+    print("Valency of",name,"is----> -1")
+elif name == ("S"):
+    print("Name of", name , "is Sulphide ion")
+    print("Valency of",name,"is----> -2")
+elif name == ("P"):
+    print("Name of", name , "is Phosphide ion")
+    print("Valency of",name,"is----> -2")
+elif name == ("No3"):
+    print("Name of", name , "is Nitrate ion")
+    print("Valency of",name,"is----> -1")
+elif name == ("No2"):
+    print("Name of", name , "is Nitrite ion")
+    print("Valency of",name,"is----> -1")
+elif name == ("Sn"):
+    print("Name of this ion when its valency is +2 that is Stannace ion")
+    print("Name of this ion when its valency is +4 that is Stannic ion")
+elif name == ("Pb"):
+    print("Name of this ion when its valency is +2 that is Plumbous ion")
+    print("Name of this ion when its valency is +4 that is Plumbic ion")
+elif name == ("MnO4"):
+    print("Name of", name , "is Per Magnet ion")
+    print("Valency of",name,"is----> -1")
+elif name == ("CR2O7"):
+    print("Name of", name , "is Dichromate ion")
+    print("Valency of",name,"is----> -1")
+elif name == ("K"):
+    print("Name of", name , "is Potassium ion")
+    print("Valency of",name,"is----> +1")
+elif name == ("Ca"):
+    print("Name of", name , "is Calcium ion")
+    print("Valency of",name,"is----> +2")
+elif name == ("Mg"):
+    print("Name of", name , "is Mgnesium ion")
+    print("Valency of",name,"is----> +2")
+elif name == ("Li"):
+    print("Name of", name , "is Lithium ion")
+    print("Valency of",name,"is----> +1")
+elif name == ("Zn"):
+    print("Name of", name , "is Zinc ion")
+    print("Valency of",name,"is----> +2")
+elif name == ("Mn"):
+    print("Name of", name , "is Magnese ion")
+    print("Valency of",name,"is----> +2")
+elif name == ("F"):
+    print("Name of", name , "is Fluoride ion")
+    print("Valency of",name,"is----> -1")
+elif name == ("CH3COO"):
+    print("Name of", name , "is Acetate ion")
+    print("Valency of",name,"is----> -1")
+elif name == ("Hg"):
+    print("Name of this ion when its valency is +1 that is Mercurous ion")
+    print("Name of this ion when its valency is -2 that is Mercuric ion")
+elif name == ("Br"):
+    print("Name of", name , "is Bromide ion")
+    print("Valency of",name,"is----> -1")
+elif name == ("I"):
+    print("Name of", name , "is Iodide ion")
+    print("Valency of",name,"is----> -1")
+elif name == ("C6H5COO"):
+    print("Name of", name , "is Benzoate ion")
+    print("Valency of",name,"is----> -1")
+elif name == ("O"):
+    print("Name of", name , "is Oxide / Monoxide ion")
+    print("Valency of",name,"is----> -2")
+elif name == ("O2"):
+    print("Name of this ion when its valency is -1 that is Super Oxide ion")
+    print("Name of this ion when its valency is -2 that is Peroxide ion")
+else:
+    print("Error! Please Check Your Input!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ square of a number.py
@@ -0,0 +1,7 @@
+num = int(input("enter your number:\n"))
+
+if num == (num):
+    print("square number was" , num , "your answer is " , (num*num)    )
+    print(f"{num}{'='}{num*num}")
+else:
+    print("Thanks")
